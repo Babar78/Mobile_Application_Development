2e1784aef55c4bc315fe6cb2e93f02a12228249b
Babar78 <muhammadbabarashraf8@gmail.com>
2024-04-20 18:27:25 +0500

    Changes

---
+++ Chapter-5-Navigation-Task/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import './login_screen.dart';
+import './stopwatch.dart';
+
+void main() => runApp(const StopwatchApp());
+
+class StopwatchApp extends StatelessWidget {
+  const StopwatchApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      routes: {
+        '/': (context) => const LoginScreen(),
+        LoginScreen.route: (context) => const LoginScreen(),
+        StopWatch.route: (context) => const StopWatch(),
+      },
+      initialRoute: '/',
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
