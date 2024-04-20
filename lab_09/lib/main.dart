0ec02b0cc89a7ebe09781a4b04da0821d4a7c0a0
Babar78 <muhammadbabarashraf8@gmail.com>
2024-04-20 19:47:11 +0500

    Task 02

--- lab_09/lib/main.dart
+++ lab_09/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:lab_09/models/todo.dart';
+import 'package:lab_09/services/todo_service.dart';
 
 void main() {
   runApp(const MyApp());
@@ -27,66 +29,114 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  fetchUsername() async {
-    return Future.delayed(
-       const Duration(
-          seconds: 3,
-        ),
-        () => 'Muhammad Babar');
-  }
-
-  addHello(user) {
-    return 'Hello $user';
-  }
-
-  greetUser() async {
-    String username = await fetchUsername();
-    return addHello(username);
-  }
-
-  sayGoodbye() async {
-    try {
-      String result = await logoutUser();
-      return '$result Thanks, see you next time';
-    } catch (e) {
-      return 'Caught error: ${e.toString()}';
-    }
-  }
-
-  logoutUser() async {
-    return Future.delayed(const Duration(milliseconds: 500), () => 'Muhammad Babar');
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Task 1 (Part 03)',
-            style: TextStyle(color: Colors.white)),
+        title: const Text(
+          'Task 02 (API Response)',
+          style: TextStyle(color: Colors.white),
+        ),
         backgroundColor: Colors.blue,
       ),
+      backgroundColor: Colors.grey[20],
       body: Center(
         child: FutureBuilder(
-          future: greetUser(),
-          builder: (context, snapshot) =>
-              snapshot.connectionState == ConnectionState.waiting
-                  ? const CircularProgressIndicator(
-                      color: Colors.blue,
+          future: TodoService().fetchTodo(),
+          builder: (context, snapshot) => snapshot.connectionState ==
+                  ConnectionState.waiting
+              ? const CircularProgressIndicator(
+                  color: Colors.blue,
+                )
+              : snapshot.connectionState == ConnectionState.done
+                  ? ListView.builder(
+                      itemCount: (snapshot.data as List<Todo>).length,
+                      itemBuilder: (context, index) {
+                        List<Todo> todoList = snapshot.data as List<Todo>;
+                        return Padding(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 24.0, vertical: 8.0),
+                          child: Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(8),
+                              color: Colors.white,
+                              border: Border.all(
+                                color: const Color.fromARGB(255, 200, 200, 200),
+                                width: 1,
+                              ),
+                            ),
+                            child: Column(
+                              children: [
+                                SizedBox(
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Row(
+                                      children: [
+                                        const Text(
+                                          'ID: ',
+                                          style: TextStyle(
+                                              color: Colors.blue,
+                                              fontWeight: FontWeight.bold),
+                                        ),
+                                        Padding(
+                                          padding:
+                                              const EdgeInsets.only(left: 16.0),
+                                          child: Text(
+                                              todoList[index].id.toString(),
+                                              style: const TextStyle(
+                                                  fontWeight: FontWeight.bold)),
+                                        )
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                                SizedBox(
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Row(
+                                      children: [
+                                        const Text(
+                                          'Title: ',
+                                          style: TextStyle(
+                                              color: Colors.blue,
+                                              fontWeight: FontWeight.bold),
+                                        ),
+                                        Expanded(
+                                          child: Padding(
+                                            padding: const EdgeInsets.only(
+                                                left: 16.0),
+                                            child: Text(
+                                              _truncateTitle(
+                                                  todoList[index].title),
+                                              style: const TextStyle(
+                                                  fontWeight: FontWeight.bold),
+                                            ),
+                                          ),
+                                        )
+                                      ],
+                                    ),
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                        );
+                      },
                     )
-                  : snapshot.connectionState == ConnectionState.done
-                      ? Text('${snapshot.data}')
-                      : const Text('Error'),
+                  : const Text('error'),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Colors.blue,
-        onPressed: () async {
-          String message = await sayGoodbye();
-          ScaffoldMessenger.of(context)
-              .showSnackBar(SnackBar(content: Text(message)));
-        },
-        child: const Icon(Icons.add),
       ),
     );
   }
+}
+
+String _truncateTitle(String title) {
+  // Split the title into words
+  List<String> words = title.split(' ');
+
+  // Take the first 5 words (or less if the title has fewer than 5 words)
+  List<String> truncatedWords = words.length <= 5 ? words : words.sublist(0, 5);
+
+  // Join the truncated words back together
+  return truncatedWords.join(' ');
 }
