7a1fc2512350073dfc01be12162f41829b493e70
Babar78 <muhammadbabarashraf8@gmail.com>
2024-04-23 16:51:43 +0500

    Lab 10 - Task 1 (Part 2) Complete

--- lab_10/lib/models/search.dart
+++ lab_10/lib/models/search.dart
@@ -16,21 +16,24 @@ class Search {
 
   factory Search.fromJson(Map<String, dynamic> json) {
     return Search(
-      tags: List<String>.from(json['tags']),
-      url: json['url'],
-      contentType: json['contentType'],
-      title: json['title'],
-      date: json['date'],
+      tags: (json['tags'] as List<dynamic>?)
+              ?.map((tag) => tag.toString())
+              .toList() ??
+          [],
+      url: json['url'].toString(),
+      contentType: json['contentType'].toString(),
+      title: json['title'].toString(),
+      date: json['date'].toString(),
     );
   }
 
   Map<String, dynamic> toJson() {
     return {
-      'tags': tags,
-      'url': url,
-      'contentType': contentType,
-      'title': title,
-      'date': date,
+      'tags': tags.map((tag) => tag.toString()).toList(),
+      'url': url.toString(),
+      'contentType': contentType.toString(),
+      'title': title.toString(),
+      'date': date.toString(),
     };
   }
 }
